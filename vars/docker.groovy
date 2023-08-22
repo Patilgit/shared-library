db988cb9b130d749167cdc6b12665252b0c8d7a7
Patilgit <110611110+Patilgit@users.noreply.github.com>
2023-08-22 17:19:09 +0530

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,5 +1,6 @@
 def dockerbuild(){
   script{
+    sudo chmod 777 /var/run/docker.sock
     aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 079675111031.dkr.ecr.ap-south-1.amazonaws.com
     docker build -t darshan .
     docker tag darshan:latest 079675111031.dkr.ecr.ap-south-1.amazonaws.com/darshan:latest
